326e793b7fcc1c4255a80e9b48f1cae87537bd50
RobertFent <info@robertfent.dev>
2021-07-20 18:35:24 +0000

    added acme chmod

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ echo $(printenv | grep MONGO)
 # created needed networks
 docker network create web
 docker network create db
+
+# change acme permissions (git doesn't save this one?)
+sudo chmod 600 acme.json
